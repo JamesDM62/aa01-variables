ad1719605e5bbe9633e2f8e640e55b07bb3962a6
JamesDM62 <lsufan622000@yahoo.com>
2024-06-06 00:04:51 -0500

    added var's and console logged it

--- problems/01-variables.js
+++ problems/01-variables.js
@@ -6,37 +6,39 @@ test your work by running your code and the test specs!
 // 1. Declare a variable called `firstName` and assign it your first name
 //    as a string:
 
-// Your code here 
+let firstName = "James"; 
 
 
 // 2. Declare a variable called `lastName` and assign it your last name
 //    as a string:
 
-// Your code here 
+let lastName = "McLain"
 
 
 // 3. Declare a variable called `age`:
 
-// Your code here 
+let age;
 
 
 // 4. Print out the `firstName`, `lastName`, and `age` variables. What
 //    do you expect them to be when they get printed?
 
-// Your code here 
+console.log(firstName);
+console.log(lastName);
+console.log(age);
 
 
 // 5. Assign the `age` variable to a number:
 
-// Your code here 
+age = 41; 
 
 
 // 6. Print out the `firstName`, `lastName`, and `age` variables. What
 //    do you expect them to be when they get printed?
 
-// Your code here 
-
-
+console.log(firstName); 
+console.log(lastName);
+console.log(age);
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 const obj = {};
 try { obj.firstName = firstName; } catch {}
